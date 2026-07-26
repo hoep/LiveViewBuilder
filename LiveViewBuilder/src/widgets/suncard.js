933ac83a699038c78ab79c71168b4b99d0ce7198
Peter Höllwarth <peter.hoellwarth@gmail.com>
2026-07-26 19:56:59 +0200

    Sonnenbogen: optionale Nachtdarstellung - gespiegelte Nachtkurve unter dem Horizont mit Mond, der nach Sonnenuntergang von Untergang zu Aufgang wandert (Schalter 'Nacht + Mond', Standard aus)

--- LiveViewBuilder/src/widgets/suncard.js
+++ LiveViewBuilder/src/widgets/suncard.js
@@ -2,8 +2,14 @@
   defWidget('suncard',{
     label:'Sonnenbogen', paletteIcon:'sunrise', size:[280,150],
     defaults:function(w){w.label='Sonne';},
-    render:function(w){return '<div class="hsc"><div class="hscarcwrap"><svg class="hscsvg" viewBox="0 0 200 96" preserveAspectRatio="none"><path class="hscarc" d="M12 82 Q100 -6 188 82"/><line class="hschoriz" x1="0" y1="82" x2="200" y2="82"/><circle class="hscsun" data-role="sun" cx="12" cy="82" r="7"/></svg>'+(w.showTime?'<div class="hscnow" data-role="now"></div>':'')+'</div><div class="hscrow"><span class="hsct"><svg class="scic" viewBox="0 0 24 24">'+ICONS.sunrise[1]+'</svg><span data-role="val">–</span></span><span class="hsclen" data-role="len"></span><span class="hsct"><svg class="scic" viewBox="0 0 24 24">'+ICONS.sunset[1]+'</svg><span data-role="val2">–</span></span></div></div>';},
-    props:function(w){return row('Uhrzeit anzeigen','<input type="checkbox" id="pSunTime"'+(w.showTime?' checked':'')+'>');},
-    wire:function(w){if($('#pSunTime'))$('#pSunTime').onchange=function(){w.showTime=this.checked||undefined;render();commit();};},
+    render:function(w){
+      var svg=w.showNight
+        ? '<svg class="hscsvg" viewBox="0 0 200 128" preserveAspectRatio="none"><path class="hscarc" d="M12 64 Q100 -4 188 64"/><path class="hscarc hscarcn" d="M188 64 Q100 132 12 64"/><line class="hschoriz" x1="0" y1="64" x2="200" y2="64"/><g class="hscmoon" data-role="moon" transform="translate(188,64)"><path d="M4 -5.2 A6 6 0 1 0 4 5.2 A4.6 4.6 0 1 1 4 -5.2 Z"/></g><circle class="hscsun" data-role="sun" cx="12" cy="64" r="7"/></svg>'
+        : '<svg class="hscsvg" viewBox="0 0 200 96" preserveAspectRatio="none"><path class="hscarc" d="M12 82 Q100 -6 188 82"/><line class="hschoriz" x1="0" y1="82" x2="200" y2="82"/><circle class="hscsun" data-role="sun" cx="12" cy="82" r="7"/></svg>';
+      return '<div class="hsc"><div class="hscarcwrap">'+svg+(w.showTime?'<div class="hscnow" data-role="now"></div>':'')+'</div><div class="hscrow"><span class="hsct"><svg class="scic" viewBox="0 0 24 24">'+ICONS.sunrise[1]+'</svg><span data-role="val">–</span></span><span class="hsclen" data-role="len"></span><span class="hsct"><svg class="scic" viewBox="0 0 24 24">'+ICONS.sunset[1]+'</svg><span data-role="val2">–</span></span></div></div>';},
+    props:function(w){return row('Uhrzeit anzeigen','<input type="checkbox" id="pSunTime"'+(w.showTime?' checked':'')+'>')
+      +row('Nacht + Mond','<input type="checkbox" id="pSunNight"'+(w.showNight?' checked':'')+'>');},
+    wire:function(w){if($('#pSunTime'))$('#pSunTime').onchange=function(){w.showTime=this.checked||undefined;render();commit();};
+      if($('#pSunNight'))$('#pSunNight').onchange=function(){w.showNight=this.checked||undefined;render();refreshSun(w);commit();};},
     live:function(w,el,id,d,base,txt,on){refreshSun(w);return;}
   });
